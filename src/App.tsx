ef5c7edaed0ccb9bf26ddeb6b1db4089b0b052cd
Elio1995 <mehmetielio95@gmail.com>
2022-08-19 11:47:01 +0200

    Added background color and dark them image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import darkImg from "../images/bg-desktop-dark.jpg";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <img style={{ width: "100vw" }} src={darkImg}></img>
+      <div className="App"></div>
+    </>
+  );
 }
 
 export default App;
