61e7d2d152cba23dcb1fbc03cd7483f8dbcea5f1
Elio1995 <mehmetielio95@gmail.com>
2022-09-07 11:46:03 +0200

    Added the function of posting todos in the list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import darkImg from "../images/bg-desktop-dark.jpg";
+
 import "./App.css";
 
 interface TIME {
@@ -9,6 +10,9 @@ interface TIME {
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const todoRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [validated, setValidated] = useState(false);
 
   function getTodoList() {
     return fetch(`http://localhost:3000/todo`)
@@ -16,6 +20,44 @@ function App() {
       .then((data) => {
         setTodos(data);
       });
+  }
+
+  function handleSubmit(e: any) {
+    const data: TIME = {
+      id: Math.random() * (1000 - 1) + 1,
+      name: todoRef.current.value,
+    };
+
+    fetch("http://localhost:3000/todo", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+
+    // const form = e.currentTarget;
+    // e.preventDefault();
+    // e.stopPropagation();
+
+    // setLoading(true);
+
+    // let uD = JSON.parse(JSON.stringify(todos));
+
+    // uD["id"] = Math.random() * (1000 - 1) + 1;
+    // uD["name"] = todoRef.current.value;
+
+    // // setTimeout(() => {
+    // setValidated(true);
+    // setLoading(false);
+    // // }, 2000);
   }
   useEffect(() => {
     getTodoList();
@@ -39,16 +81,23 @@ function App() {
             <p style={{ textAlign: "end" }}>TODO</p>
           </div>
           <div>
-            <input
-              style={{
-                width: "100%",
-                height: "50px",
-                backgroundColor: "hsl(235, 24%, 19%)",
-                border: "none",
-                marginBottom: "30px",
-              }}
-              type="text"
-            ></input>
+            <form noValidate onSubmit={handleSubmit}>
+              <label htmlFor="html">HTML</label>
+              <input
+                style={{
+                  width: "100%",
+                  height: "50px",
+                  backgroundColor: "hsl(235, 24%, 19%)",
+                  border: "none",
+                  marginBottom: "30px",
+                }}
+                type="text"
+                ref={todoRef}
+              ></input>
+              <button disabled={loading} type="submit">
+                Add Todo
+              </button>
+            </form>
           </div>
           <div>
             {todos.map((todo: TIME) => {
